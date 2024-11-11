b7c6abaeeff72c7354e627391c824460f743f57c
Atanas Vasilev <atanas.vasilev1992@gmail.com>
2024-11-11 14:41:05 +0200

    Add HTML & CSS in main

--- explore-bg-app/src/app/core/core.module.ts
+++ explore-bg-app/src/app/core/core.module.ts
@@ -14,6 +14,11 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
